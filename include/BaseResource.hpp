22a50ec3f5fb07e7ef55203dbcc262305b121fc5
JimVincent <1000343@student.sae.edu.au>
2014-12-08 21:03:01 +1000

    BaseResource .hpp mostly complete
    Needs more comments and some final touches.

--- include/BaseResource.hpp
+++ include/BaseResource.hpp
@@ -29,9 +29,80 @@
 // Headers
 ////////////////////////////////////////////////////////////
 
+#include <iostream>
+#include <string>
+
+
 namespace rm 
 {
+    ///////////////////////////////////////////////////////////
+    /// \brief An abstract resource class 
+    /// 
+    /// This abstract base class needs to be inherited by a
+    /// ManagedResource class. It contains three pure virtual
+    /// functions that require a specific body of code for each
+    /// resource type.
+    ///
+    ///////////////////////////////////////////////////////////
+	class BaseResource
+	{
+    public:
+        void BaseResource();
+        void ~BaseResource();
 
+        ///////////////////////////////////////////////////////////
+        /// \brief Loads the resource from the file path
+        ///
+        /// pure virtual function that has a specific load
+        /// function body for each resource type.
+        ///
+        ///////////////////////////////////////////////////////////
+        virtual void load() = 0;
+
+        ///////////////////////////////////////////////////////////
+        /// \brief unloads the resource
+        ///
+        /// a pure virtual function that has a specific unload
+        /// function body for each resource type.
+        ///
+        ///////////////////////////////////////////////////////////
+        virtual void unload() = 0;
+
+        ///////////////////////////////////////////////////////////
+        /// \brief Reloads the resource from the file path
+        ///
+        /// a pure virtual function that has a specific reload 
+        /// function body for each resource type.
+        ///
+        ///////////////////////////////////////////////////////////
+        virtual void reload() = 0;
+
+        ///////////////////////////////////////////////////////////
+        /// \brief Returns the alias of type string by value
+        ///
+        ///////////////////////////////////////////////////////////
+        std::string getAlias()const;
+
+        ///////////////////////////////////////////////////////////
+        /// \ brief 
+        ///
+        ///////////////////////////////////////////////////////////
+        std::string getResourceType()const;
+        std::string getFilePath()const;
+        bool isLoaded()const;
+        size_t getRamUse()const;
+
+        void setAlias();
+        void setResourceType();
+        void setFilePath();
+        void setIsLoaded();
+        void setRam();
+        
+    private:
+        std::string alias, type, filePath;
+        bool isLoaded;
+        size_t ramUse;
+	};
 }
 
 
