21c0777aa7e553e4cd0ec83554f088ac6e0f429b
Amaranthos <joshua.hodkinson.42@gmail.com>
2014-12-09 10:36:39 +1000

    Completed ManagedMusic.hpp, ManagedFont.hpp and ManagedTexture.hpp, all may still need refining

--- include/SFML/ManagedTexture.hpp
+++ include/SFML/ManagedTexture.hpp
@@ -29,12 +29,97 @@
 // Headers
 ////////////////////////////////////////////////////////////
 
+#include <SFML/Graphic/Texture.hpp>
+
 #include <BaseResource.hpp>
 
-////////////////////////////////////////////////////////////
-///
-///
-////////////////////////////////////////////////////////////
 
+namespace rm
+{
+    ////////////////////////////////////////////////////////////
+    /// \brief An inherited resource class for SFML Texture types
+    /// 
+    /// This class handles an SFML Texture resource type, with specific
+    /// implementation for loading, unloading and reloading.
+    ////////////////////////////////////////////////////////////
+    
+    class ManagedTexture
+    {
+    public:
+        
+        ////////////////////////////////////////////////////////////
+        /// \brief Default Constructor
+        /// 
+        /// Normally called by rm::ResourceFactory
+        ////////////////////////////////////////////////////////////
+        void ManagedTexture();
+        
+        ////////////////////////////////////////////////////////////
+        /// \brief  Destructor
+        /// 
+        /// Standard destructor
+        ////////////////////////////////////////////////////////////
+        void ~ManagedTexture();
+        
+        ////////////////////////////////////////////////////////////
+        /// \brief Loads the SFML Texture resource
+        /// 
+        /// Invoked when rm::ResourceManager::singleton::load() is called
+        ////////////////////////////////////////////////////////////
+        void load();
+        
+        ////////////////////////////////////////////////////////////
+        /// \brief Unloads the SFML Texture resource, 
+        /// but doesn't remove the instance of this class from rm::ResourceManager
+        /// 
+        /// Invoked when rm::ResourceManager::singleton::unload() is called
+        ////////////////////////////////////////////////////////////
+        void unload();
+        
+        ////////////////////////////////////////////////////////////
+        /// \brief Force reloads the SFML Texture resource
+        /// 
+        /// Invoked when rm::ResourceManager::singleton::reload() is called
+        ////////////////////////////////////////////////////////////
+        void reload();
+        
+        ////////////////////////////////////////////////////////////
+        /// \brief Returns the resource type of this instance
+        ////////////////////////////////////////////////////////////
+        string& getResourceClassType();
+        
+        ///////////////////////////////////////////////////////////
+        /// \brief Returns the size of memory 
+        /// 
+        /// Helper function so that memory allocation can be kept track of
+        ////////////////////////////////////////////////////////////
+        size_t getRamUse();
+        
+        ////////////////////////////////////////////////////////////
+        /// \brief Returns a pointer to the actual Texture resource
+        ////////////////////////////////////////////////////////////
+        sf::Texture* getTexture();
+        
+    private:
+    
+        ////////////////////////////////////////////////////////////
+        /// Member data
+        ////////////////////////////////////////////////////////////
+        const sf::Texture*    m_texture;    ///< Reference to the Texture resource
+    };
+
+} // namespace rm
 
 #endif //MANAGED_TEXTURE_HPP
+
+////////////////////////////////////////////////////////////
+/// \class ManagedTexture
+/// \ingroup
+/// rm::ManagedTexture is the inherited class which handles loading and unloading of 
+/// SFML sf::Texture resources. rm::ManagedTexture implements rm::BaseResource.
+///
+/// Usage example:
+/// \code
+/// (rm::ManagedTexture) rm::ResourceManager::singleton::getResource("example_Texture")->getTexture()->play();
+/// \endcode
+////////////////////////////////////////////////////////////
