7275b95cc295709720aaab3450f70ddc5945c406
JimVincent <1000343@student.sae.edu.au>
2014-12-11 13:49:06 +1000

    Updated BaseResource.cpp
    Corrected comments and layout.

--- src/ResourceManager/BaseResource.cpp
+++ src/ResourceManager/BaseResource.cpp
@@ -1,3 +1,33 @@
+////////////////////////////////////////////////////////////
+//
+// The MIT License (MIT)
+//
+// Copyright (c) 2014 stevehalliwell
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////
+// Headers
+////////////////////////////////////////////////////////////
+
 #include <BaseResource.hpp>
 
 
@@ -12,66 +42,40 @@ public:
     {
     }
 
-    ///////////////////////////////////////////////////////////
-    /// \brief Returns the alias of type string
-    ///
-    ///////////////////////////////////////////////////////////
+    // getters
+
     std::string BaseResource::getAlias()const
     {
         return alias;
     }
 
-    ///////////////////////////////////////////////////////////
-    /// \brief Returns the resource type string
-    ///
-    ///////////////////////////////////////////////////////////
     std::string BaseResource::getResourceType()const
     {
         return type;
     }
 
-    ///////////////////////////////////////////////////////////
-    /// \brief Returns the file path string
-    ///
-    ///////////////////////////////////////////////////////////
     std::string BaseResource::getFilePath()const
     {
         return filePath;
     }
 
-    ///////////////////////////////////////////////////////////
-    /// \brief Returns true if resource is loaded
-    ///
-    ///////////////////////////////////////////////////////////
+    // setters
+
     bool BaseResource::isLoaded()const
     {
         return isResourceLoaded;
     }
 
-    ///////////////////////////////////////////////////////////
-    /// \brief Sets the alias member
-    ///
-    /// \param newalias The new Alias
-    ///
-    ///////////////////////////////////////////////////////////
     void BaseResource::setAlias(const std::string& newAlias)
     {
         alias = newAlias;
     }
 
-    ///////////////////////////////////////////////////////////
-    /// \brief Sets the file path member
-    ///
-    ///////////////////////////////////////////////////////////
     void BaseResource::setFilePath(const std::string& newFilePath)
     {
         filePath = new filePath;
     }
 
-    ///////////////////////////////////////////////////////////
-    /// \brief Sets the isLoaded member
-    ///
-    ///////////////////////////////////////////////////////////
     void BaseResource::setIsLoaded(bool isLoaded)
     {
         isResourceLoaded = isLoaded;
