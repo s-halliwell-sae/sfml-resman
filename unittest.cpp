49022e1776763d62745c2d42ec5ce54119052e4c
manpat <pat@patsown.com>
2014-12-12 10:06:27 +1000

    Went through ResourceManager::loadResource

--- unittest.cpp
+++ unittest.cpp
@@ -4,18 +4,42 @@
 
 class Test : public rm::BaseResource {
 public:
+	bool load() override {
+		return true;
+	}
+	bool unload() override {
+		return true;
+	}
+	bool reload() override {
+		return true;
+	}
+
+	size_t getMemUsage() const override {
+		return sizeof(Test);
+	}
+
 	static std::string getResourceClassType(){
-		return "test";
+		return "text";
 	}
 };
 
-int main(){
+void unittest(){
 	std::string iAmAString = "is ";
 	char c = 'a';
 
 	rm::Logger::setFileLocation("log.txt");
 	rm::Logger::logMessage("Hello");
-	rm::Logger::logMessage("Hello ", "this ", iAmAString, c, "Thing ", 1234);
+	rm::Logger::logMessage("Hello ", "this ", iAmAString, c, " Thing ", 1234);
+
+	rm::ResourceManager::createErrorResource<Test>("log.txt");
+}
+
+int main(){
+	try{
+		unittest();
+	}catch(const char* e){
+		rm::Logger::logMessage("Exception: ", e);
+	}
 
 	return 0;
 }
