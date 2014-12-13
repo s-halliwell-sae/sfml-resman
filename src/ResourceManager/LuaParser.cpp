a5b2d01d01ac372aa9fa394cf0eb9368ee25c7a1
volp <theinsanelygreat@hotmail.com>
2014-12-13 17:54:37 +1000

    Added first implementation of LuaParser without comments

--- src/ResourceManager/LuaParser.cpp
+++ src/ResourceManager/LuaParser.cpp
@@ -33,20 +33,109 @@ namespace rm
 /// Initialise static members
 ////////////////////////////////////////////////////////////
 
-luaState* ResourceManager::m_luaState = null;
+lua_State* ResourceManager::m_luaState = nullptr;
 
 ////////////////////////////////////////////////////////////
 /// Function definitions
 ////////////////////////////////////////////////////////////
 
-ResourceDataList ResourceManager::parsePack(const std::string& path)
+static std::string stringify(lua_State* L, int index)
 {
-	throw("Not Implemented");
+    if (lua_isstring(L, index))
+        return lua_tostring(L, index);
+    else
+    {
+        int type = lua_type(L, index);
+
+        if (type == LUA_TBOOLEAN)
+            return std::string(lua_toboolean(L, index) ? "true" : "false");
+        else
+            return std::string(lua_typename(L, type));
+    }
 }
 
-ResourceDataList ResourceManager::leafPack(const ResourceData& data)
+ResourceDataList ResourceManager::parsePack(const std::string& path)
 {
-	throw("Not Implemented");
+    ResourceDataList resourceDataList;
+    std::vector<std::string> resourcePacks = {path};
+
+    for (auto i = 0; i < resourcePacks.size(); i++)
+    {
+        ResourceDataList leaves = leafPack(resourcePacks[i]);
+
+        for (auto j = leaves.begin(); j != leaves.end(); j++)
+        {
+            if (j->isResourcePack)
+            {
+                if (std::find(resourcePacks.begin(), resourcePacks.end(), j->path) != resourcePacks.end())
+                    logMessage("Cyclic dependency detected");
+                else
+                    resourcePacks.push_back(j->path);
+            }
+            else
+                resourceDataList.push_back(*j);
+        }
+    }
+
+    return resourceDataList;
+}
+
+ResourceDataList ResourceManager::leafPack(const std::string& path)
+{
+    ResourceDataList resourceDataList;
+
+    m_luaState = luaL_newstate();
+    luaL_openlibs(m_luaState);
+
+    if (luaL_dofile(m_luaState, path.data()) != 0)
+        logMessage(lua_tostring(m_luaState, -1));
+    else
+    {
+        lua_pushnil(m_luaState);
+
+        while (lua_next(m_luaState, -2) != 0)
+        {
+            lua_pushnil(m_luaState);
+            resourceDataList.push_back(parseLeaf());
+            lua_pop(m_luaState, 1);
+        }
+
+        lua_close(m_luaState);
+    }
+
+    m_luaState = nullptr;
+
+    return resourceDataList;
+}
+
+ResourceData ResourceManager::parseLeaf()
+{
+    ResourceData resourceData;
+    resourceData.isResourcePack = false;
+
+    while (lua_next(m_luaState, -2) != 0)
+    {
+        std::string key   = stringify(m_luaState, -2);
+        std::string value = stringify(m_luaState, -1);
+
+        if (key == "path")
+            resourceData.path = value;
+        else if (key == "type")
+        {
+            if ( value == "resourcepack" )
+                resourceData.isResourcePack = true;
+
+            resourceData.type = value;
+        }
+        else if (key == "alias")
+        {
+            resourceData.alias = value;
+        }
+
+        lua_pop(m_luaState, 1);
+    }
+
+    return resourceData;
 }
 
 } // rm
