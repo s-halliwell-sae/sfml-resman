11f4ba0b4b2c771e3aa0bae71f2c43cca275a0e0
volp <theinsanelygreat@hotmail.com>
2014-12-17 07:20:27 +1000

    Fixed invalid include

--- include/ResourceManager/LuaParser.hpp
+++ include/ResourceManager/LuaParser.hpp
@@ -32,7 +32,7 @@
 ////////////////////////////////////////////////////////////
 
 #include <ResourceManager/ResourceData.hpp>
-#include <ResourceManager\Logger.hpp>
+#include <ResourceManager/Logger.hpp>
 #include <string>
 #include <list>
 #include <vector>
